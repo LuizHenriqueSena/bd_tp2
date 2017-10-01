6019ddd96e5a3a947f9022837d1c50d74d2c885e
LuizHenriqueSena <coelho.luiz.sena@gmail.com>
2017-10-01 16:40:51 -0400

    Atualizando para receber os parametros do usuario

--- criaTabela.cpp
+++ criaTabela.cpp
@@ -6,9 +6,15 @@ using namespace pqxx;
 
 int main(int argc, char* argv[]) {
    char * sql;
-   
+   string dbname, nome, password;
+   cout<<"Digite o nome do bd:";
+	getline(cin,dbname);
+   cout<<"Digite o nome do usuario:";
+	getline(cin,nome);
+   cout<<"Digite a senha do usuario:";
+	getline(cin,password);
    try {
-      connection C("dbname = luiz user = luiz password = luiz \
+      connection C("dbname =" +dbname + " user = "+ nome +" password = "+password+" \
       hostaddr = 127.0.0.1 port = 5432");
       if (C.is_open()) {
          cout << "Opened database successfully: " << C.dbname() << endl;
@@ -21,17 +27,17 @@ int main(int argc, char* argv[]) {
       work W(C);
       
       /* Create the SQL query */
-      //sql = "CREATE TABLE Produtos("  \
-      //"id INT PRIMARY KEY     NOT NULL," \
-      //"asin           TEXT    NOT NULL," \
-      //"title          TEXT    NOT NULL," \
-      //"grupo          TEXT    NOT NULL," \
-      //"sales_rank     INT     NOT NULL);";
+      sql = "CREATE TABLE Produtos("  \
+      "id INT PRIMARY KEY     NOT NULL," \
+      "asin           TEXT    NOT NULL," \
+      "title          TEXT    NOT NULL," \
+      "grupo          TEXT    NOT NULL," \
+      "sales_rank     INT     NOT NULL);";
 
       /* Execute SQL query */
-      //W.exec( sql );
+      W.exec( sql );
      // W.commit();
-      //cout << "Table Produtos created successfully" << endl;
+      cout << "Table Produtos created successfully" << endl;
 
       /* Create the SQL query */
       sql = "CREATE TABLE Similares("  \
